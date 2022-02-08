f8c934fde78e0f290af1990b02e18c5ae913331f
RenanMoreira92 <renan.s.7@hotmail.com>
2022-02-08 12:23:33 -0300

    Teste inicial dias

---
+++ src/test/java/com/example/tdd/BookingServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.tdd;
+
+import com.example.tdd.service.BookingService;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class BookingServiceTest {
+
+    @TestConfiguration
+    static class BookingServiceTestConfiguration {
+        @Bean
+        public BookingService bookingservice() {
+            return new BookingService();
+        }
+
+    }
+
+    @Autowired
+    BookingService bookingService;
+
+    @Test
+    public void bookingTestServiceDaysCalculator() {
+        String name = "Renan";
+        int days = bookingService.daysCalculatorWithDatabase(name);
+
+        Assertions.assertEquals(days,10);
+    }
+}
